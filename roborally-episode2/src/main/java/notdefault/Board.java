6b84c87297ba18d3da1a1c9ba57ee625e8e34dc5
amiralimov <amiralimov@amirs-mbp-2>
2022-03-30 23:02:24 +0200

    Spawning with direction

--- roborally-episode2/src/main/java/notdefault/Board.java
+++ roborally-episode2/src/main/java/notdefault/Board.java
@@ -3,71 +3,83 @@ package notdefault;
 import java.util.ArrayList;
 
 public class Board {
-	private int rows;
-	private int cols;
-	private String[][] board;
-	private ArrayList<Robot> robots = new ArrayList<Robot>();
-	private ArrayList<int[]> corners = new ArrayList<int[]>();
+    private int rows;
+    private int cols;
+    private String[][] board;
+    private ArrayList<Robot> robots;
+    private ArrayList<int[]> corners = new ArrayList<int[]>();
 
 
-	public Board(int int1, int int2) {
-		this.rows = int1;
-		this.cols = int2;
-		this.board = new String[cols][rows];
-		corners.add(new int[] {0,0});
-		corners.add(new int[] {rows-1,cols-1});
-		corners.add(new int[] {0,cols-1});
-		corners.add(new int[] {rows-1,0});
+    public Board(int int1, int int2) {
+	this.rows = int1;
+	this.cols = int2;
+	this.board = new String[cols][rows];
+	corners.add(new int[] {0,0});
+	corners.add(new int[] {rows-1,cols-1});
+	corners.add(new int[] {0,cols-1});
+	corners.add(new int[] {rows-1,0});
+    }
+
+    public int[] getSize() {
+	return new int[] {board[0].length, board.length};
+    }
+
+    public void spawnRobot(int num) {
+	robots = new ArrayList<Robot>();
+	for(int i = 0; i<num; i++) {
+	    robots.add(new Robot());
+	    robots.get(i).setCoord(corners);
+	    robots.get(i).setDir();
+	    updateCorners(robots.get(i).getCorners());
 	}
-	
-	public int[] getSize() {
-		return new int[] {board[0].length, board.length};
+    }
+    
+    public void spawnRobot(int num, int corner) {
+	robots = new ArrayList<Robot>();
+	for(int i = 0; i<num; i++) {
+	    robots.add(new Robot());
+	    robots.get(i).setCoord(corners,corner);
+	    robots.get(i).setDir();
+	    updateCorners(robots.get(i).getCorners());
 	}
-	
-	public void spawnRobot(int num) {
-		for(int i = 0; i<num; i++) {
-			robots.add(new Robot());
-			robots.get(i).setCoord(corners);
-			updateCorners(robots.get(i).getCorners());
+    }
+
+    public void updateCorners(ArrayList<int[]> c) {
+	corners = c;
+    }
+
+    public ArrayList<Robot> getRobots() {
+	return robots;
+    }
+
+    public boolean inCorner() {
+	for(int i = 0; i<robots.size(); i++) {
+	    if(robots.get(i).getX() == 0 && robots.get(i).getY() == 0) {
+		return true;
+	    } else if(robots.get(i).getX() == rows-1 && robots.get(i).getY() == cols-1) {
+		return true;
+	    } else if(robots.get(i).getX() == 0 && robots.get(i).getY() == cols-1) {
+		return true;
+	    } else if(robots.get(i).getX() == rows-1 && robots.get(i).getY() == 0) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public boolean overlap() {
+	int temp = 0;
+	for(Robot robot:robots) {
+	    for(int i = 0; i < robots.size(); i++) {
+		if(robot.getXY() == robots.get(i).getXY()) {
+		    temp++;
 		}
+	    }
 	}
-	
-	public void updateCorners(ArrayList<int[]> c) {
-		corners = c;
+	if(temp == 1) {
+	    return true;
 	}
-	
-	public ArrayList<Robot> getRobots() {
-		return robots;
-	}
-	
-	public boolean inCorner() {
-		for(int i = 0; i<robots.size(); i++) {
-			if(robots.get(i).getX() == 0 && robots.get(i).getY() == 0) {
-				return true;
-			} else if(robots.get(i).getX() == rows-1 && robots.get(i).getY() == cols-1) {
-				return true;
-			} else if(robots.get(i).getX() == 0 && robots.get(i).getY() == cols-1) {
-				return true;
-			} else if(robots.get(i).getX() == rows-1 && robots.get(i).getY() == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean overlap() {
-		int temp = 0;
-		for(Robot robot:robots) {
-			for(int i = 0; i < robots.size(); i++) {
-				if(robot.getXY() == robots.get(i).getXY()) {
-					temp++;
-				}
-			}
-		}
-		if(temp == 1) {
-			return true;
-		}
-		return false;
-	}
-	
+	return false;
+    }
+
 }
--- roborally-episode2/src/test/resources/featureFiles/boardRobotSpawn.feature
+++ roborally-episode2/src/test/resources/featureFiles/boardRobotSpawn.feature
@@ -2,21 +2,33 @@
 Feature: Robot spawning
 As a board, when a player starts the game, I generate robots
 
-	@2robotsSpawned
-  Scenario: spawn two robots
-  	Given Two 2 players
-  	When board spawns two robots
-  	Then robots are in opposite corners
-   	And one robot per corner in two corners
-  @3robotsSpawned
-  Scenario: spawn three robots
-    Given Three 3 players
-  	When board spawns three robots
-  	Then robots are in corners
-  	And one robot per corner in three corners
-  @4robotsSpawned
-  Scenario: spawn four robots
-    Given Four 4 players
-  	When board spawns four robots
-  	Then robots are in all corners
+#	@2robotsSpawned
+  #Scenario: spawn two robots
+  #	Given Two 2 players
+  #	When board spawns two robots
+  #	Then robots are in opposite corners
+   #	And one robot per corner in two corners
+  #@3robotsSpawned
+  #Scenario: spawn three robots
+    #Given Three 3 players
+  #	When board spawns three robots
+  #	Then robots are in corners
+  #	And one robot per corner in three corners
+  #@4robotsSpawned
+  #Scenario: spawn four robots
+    #Given Four 4 players
+  #	When board spawns four robots
+  #	Then robots are in all corners
+  @robotsDirection
+  Scenario: direction of robots
+    Given a corner <c>
+    When 1 robot is spawned
+    Then the directions must point to <dir>
+
+    Examples:
+		| c | dir |
+		| 0 |  2  | #2
+		| 3 |  3  | #3
+		| 2 |  0  | #0
+		| 1 |  1  | #1
   
