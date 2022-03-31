e0c074225f4082306fc4e25de07439335016c4bb
amiralimov <amiralimov@amirs-mbp-2>
2022-03-31 12:55:43 +0200

    When tile is created it is now randomly assigned a type, except checkpoint. Also does not check for robots

--- roborally-episode2/src/main/java/notdefault/Tile.java
+++ roborally-episode2/src/main/java/notdefault/Tile.java
@@ -2,4 +2,33 @@ package notdefault;
 
 public class Tile {
 
-}
+    private TileType type;
+    public Tile() {
+    }
+    
+    public void init() {
+	double val = Math.random();
+	
+	if(val< 0.65) {
+	    this.type = TileType.Floor;
+	} else if(val< 0.7) {
+	    this.type = TileType.Pit;
+	} else if(val< 0.75) {
+	    this.type = TileType.Barrel;
+	} else if(val< 0.8) {
+	    this.type = TileType.Laser;
+	} else if(val< 0.85) {
+	    this.type = TileType.Acid;
+	} else if(val< 0.9) {
+	    this.type = TileType.Health;
+	} else if(val< 0.95) {
+	    this.type = TileType.Conveyor;
+	} else {
+	    this.type = TileType.Gear;
+	}
+    }
+    
+    public TileType getType() {
+	return type;
+    }
+ }
