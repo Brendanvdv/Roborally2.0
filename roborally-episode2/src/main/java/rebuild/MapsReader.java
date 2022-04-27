ee04f04b9bbc6d04875b6188281b03dbb43c84d9
amiralimov <amiralimov@amirs-mbp-2.students.clients.local>
2022-04-27 11:17:14 +0200

    mapsreader done

--- roborally-episode2/src/main/java/rebuild/MapsReader.java
+++ roborally-episode2/src/main/java/rebuild/MapsReader.java
@@ -6,47 +6,52 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class MapsReader {
-    String[][] Map;
+    String[][] map;
     JSONParser parser;
+    
+
+    JSONArray mapJ = null;
     
     MapsReader(int diff) throws FileNotFoundException, IOException, ParseException {
 	parser = new JSONParser();
 	
 	Object obj = parser.parse(new FileReader("src/test/resources/maps/maps.json"));
 	JSONObject jsonObject = (JSONObject)obj;
-	JSONArray map;
+	
 	
 	if(diff == 1) {
-	    map = (JSONArray)jsonObject.get("easy");
+	    mapJ = (JSONArray)jsonObject.get("easy");
 	} else if(diff == 2) {
-	    map = (JSONArray)jsonObject.get("medium");
+	    mapJ = (JSONArray)jsonObject.get("medium");
 	} else if(diff == 3) {
-	    map = (JSONArray)jsonObject.get("hard");
+	    mapJ = (JSONArray)jsonObject.get("hard");
 	}
 	
-	Iterator cols = map.iterator();
+	map = new String[mapJ.size()][mapJ.size()];
 	
-	ArrayList<String[]> columns = new ArrayList<String[]>();
-	
-	
-	
-	
-	try {
-	       
-
-	       
-
-	       System.out.println("Subjects:");
-	       Iterator iterator = subjects.iterator();
-	       while (iterator.hasNext()) {
-	          System.out.println(iterator.next());
-	       }
-	    } catch(Exception e) {
-	       e.printStackTrace();
+	for(int i = 0; i < mapJ.size(); i++) {
+	    JSONArray column = (JSONArray) mapJ.get(i);
+	    
+	    for(int j = 0; j < mapJ.size(); j++) {
+		map[i][j] = (String) column.get(j);
 	    }
-	
+	}
     }
 
+    public JSONArray getMapJ() {
+        return mapJ;
+    }
+
+    public void setMapJ(JSONArray mapJ) {
+        this.mapJ = mapJ;
+    }
+    public String[][] getMap() {
+        return map;
+    }
+
+    public void setMap(String[][] map) {
+        this.map = map;
+    }
     
     
 }
--- roborally-episode2/src/main/java/rebuild/RoboRally.java
+++ roborally-episode2/src/main/java/rebuild/RoboRally.java
@@ -1,20 +1,26 @@
 package rebuild;
 
 import java.awt.Dimension;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+
+import org.json.simple.parser.ParseException;
 
 import view.GameView;
 
 public class RoboRally {
 
-    public static void main(String[] args) {
-	GameView g = new GameView();
-	
-	g.setTitle("RoboRally 4.3");
-	g.setSize(new Dimension(700, 700));
-	g.setVisible(true);
-	g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+//	GameView g = new GameView();
+//	
+//	g.setTitle("RoboRally 4.3");
+//	g.setSize(new Dimension(700, 700));
+//	g.setVisible(true);
+//	g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	
+
 	
     }
 }
